26df99d434801fa8436d06f8bce9bb18ef668bfb
Sumit Nagpal <sumitn@xilinx.com>
2020-06-23 12:26:30 +0530

    new changes from mlsuite to staging

--- alveo/apps/face_detect/detect_visual.py
+++ alveo/apps/face_detect/detect_visual.py
@@ -76,7 +76,7 @@ def faceDetection(vitis_rundir,outpath, rsz_h, rsz_w, path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'analysis densebox model')
     parser.add_argument('--vitisrundir', help = 'path to run directory ', type=str)
-    parser.add_argument('--images', help = 'path to image folder',type = str, default='test_pic/' )
+    parser.add_argument('--images', help = 'path to image folder',type = str)
     parser.add_argument('--resize_h', help = 'resize height', type = int)
     parser.add_argument('--resize_w', help = 'resize width', type = int)
     parser.add_argument('--output', help = 'output folder', type = str)
