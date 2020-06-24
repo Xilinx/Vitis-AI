f3e8e839aae144702cf54c66e704dfb3e5e74b6f
Sumit Nagpal <sumitn@xilinx.com>
2020-06-24 15:55:46 +0530

    new changes from mlsuite to staging

--- alveo/packages/install.sh
+++ alveo/packages/install.sh
@@ -158,7 +158,7 @@ wget $OVERLAYBINS_URL -O $OVERLAYBINS_INSTALLER && ${INSTALLER} install $OVERLAY
 if [[ $distroname == *"Ubuntu 16.04"* || $distroname == *"Ubuntu 18.04"* ]]; then
   wget $XPLUSML_OVERLAYBINS_URL -O $XPLUSML_OVERLAYBINS_INSTALLER && ${INSTALLER} install $XPLUSML_OVERLAYBINS_INSTALLER -y && rm $XPLUSML_OVERLAYBINS_INSTALLER
 elif [[ $distroname == *"CentOS"* || $distroname == *"Red Hat"* ]]; then
-  wget $XPLUSML_OVERLAYBINS_URL -O $XPLUSML_OVERLAYBINS_INSTALLER && rpm -ivh --force  $XPLUSML_OVERLAYBINS_INSTALLER -y && rm $XPLUSML_OVERLAYBINS_INSTALLER
+  wget $XPLUSML_OVERLAYBINS_URL -O $XPLUSML_OVERLAYBINS_INSTALLER && rpm -ivh --force  $XPLUSML_OVERLAYBINS_INSTALLER && rm $XPLUSML_OVERLAYBINS_INSTALLER
 else
   echo "Couldn't install WAA overlaybins. Unsupported operating system"
 fi
