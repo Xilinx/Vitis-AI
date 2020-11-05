5de6e446a79609f47f2b11f59aefec3d090e126f
Sumit Nagpal <sumitn@xilinx.com>
2020-11-05 04:39:39 -0700

    fall detection application for dpuv1

---
+++ examples/apps/fall_detection/kernel_src/of_classification_preprocess/AksOFClassifcationPreProcess.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include "ext/AksKernelBase.h"
+#include "ext/AksDataDescriptor.h"
+#include "ext/AksNodeParams.h"
+
+class OFClassificationPreProcess : public AKS::KernelBase
+{
+  public:
+    int exec_async (
+           std::vector<AKS::DataDescriptor*> &in, 
+           std::vector<AKS::DataDescriptor*> &out, 
+           AKS::NodeParams* nodeParams,
+           AKS::DynamicParamValues* dynParams);
+};
+
+extern "C" { /// Add this to make this available for python bindings and 
+
+AKS::KernelBase* getKernel (AKS::NodeParams *params)
+{
+  return new OFClassificationPreProcess();
+}
+
+
+int OFClassificationPreProcess::exec_async (
+           std::vector<AKS::DataDescriptor*> &in, 
+           std::vector<AKS::DataDescriptor*> &out, 
+           AKS::NodeParams* nodeParams,
+           AKS::DynamicParamValues* dynParams)
+{
+  // std::cout << "[DBG] OFClassificationPreProcess: running now ... " << std::endl;
+
+  uint8_t numDD = in.size();
+  assert(numDD > 0);
+
+  auto dd = in[0];
+  auto shape = dd->getShape();
+  int channels = shape[1];
+  int rows = shape[2];
+  int cols = shape[3];
+
+  AKS::DataDescriptor *outDD = new AKS::DataDescriptor(
+    {1, channels*numDD, rows, cols}, AKS::DataType::FLOAT32);
+  float* outData = outDD->data<float>();
+  // merge at Channels
+  for (int i=0; i<numDD*channels; i++) {
+    int8_t* data = in[i/channels]->data<int8_t>();
+    for (int j=0; j<rows; j++) {
+      for (int k=0; k<cols; k++) {
+          outData[i*rows*cols + j*cols + k] = (float)data[(i%channels)*rows*cols + j*cols + k];
+      }
+    }
+  }
+  out.push_back(outDD);
+  // std::cout << "[DBG] OFClassificationPreProcess: Done!" << std::endl << std::endl;
+  return -1; /// No wait
+}
+
+}//extern "C"
