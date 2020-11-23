280bbdb7a5303b7095611208873ffb5b270ec4a3
linqiang <linqiang@xilinx.com>
2020-11-23 18:57:44 +0800

    Update softmax-runner

--- tools/Vitis-AI-Runtime/VART/vart/softmax-runner/include/vart/softmax_runner.hpp
+++ tools/Vitis-AI-Runtime/VART/vart/softmax-runner/include/vart/softmax_runner.hpp
@@ -1,11 +1,27 @@
-// TODOL Licence
+/*
+ * Copyright 2019 Xilinx Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <memory>
 
-#include "vart/runner.hpp"
+#include "vart/runner_ext.hpp"
 #include "vart/tensor_buffer.hpp"
 #include "xir/sfm_controller.hpp"
+
 namespace vart {
-class SoftmaxRunner : public vart::Runner {
+class SoftmaxRunner : public vart::RunnerExt {
  public:
   explicit SoftmaxRunner(const xir::Subgraph* subgraph, xir::Attrs* attrs);
   SoftmaxRunner(const SoftmaxRunner& other) = delete;
@@ -19,6 +35,8 @@ class SoftmaxRunner : public vart::Runner {
   virtual int wait(int jobid, int timeout) override;
   virtual std::vector<const xir::Tensor*> get_input_tensors() override;
   virtual std::vector<const xir::Tensor*> get_output_tensors() override;
+  virtual std::vector<vart::TensorBuffer*> get_inputs() override;
+  virtual std::vector<vart::TensorBuffer*> get_outputs() override;
 
  private:
   vart::TensorBuffer* prepare_input(vart::TensorBuffer* input);
