879e74ca4cb6230083ec44a3d53ca6d996415135
Ehsan Ghasemi <ehsan.ghasemi@xilinx.com>
2020-12-09 20:21:27 -0800

    Update for TVM Vitis 1.3 release  (#143)
    * updating the zynq_setup script
    
    *  resolving the compilation issue when targetting dpuv2
    
    * removed patch file and change the zynq_setup.sh tvm installation
    
    * resolving the board setup for zcu104

--- external/tvm/zynq_setup.sh
+++ external/tvm/zynq_setup.sh
@@ -25,7 +25,7 @@ export PYXIR_HOME="${TVM_VAI_HOME}"/pyxir
 
 # DOWNLOADS
 git clone --recursive --branch v0.1.3 https://github.com/Xilinx/pyxir.git "${PYXIR_HOME}"
-git clone https://github.com/apache/incubator-tvm.git "${TVM_HOME}"
+git clone --recursive https://github.com/apache/incubator-tvm.git "${TVM_HOME}"
 
  
 # DOWNLOAD REQUIRED PYTHON PACKAGES
@@ -39,9 +39,8 @@ sudo python3 setup.py install --use_vai_rt_dpuczdx8g
 cd "${TVM_HOME}"
 mkdir "${TVM_HOME}"/build
 cp "${TVM_HOME}"/cmake/config.cmake "${TVM_HOME}"/build/
-cd "${TVM_HOME}"/build && echo set\(USE_VITIS_AI ON\) >> config.cmake && cmake .. && make -j$(nproc)
-#cd "${TVM_HOME}"/python && pip3 install -e . --user 
-cd "${TVM_HOME}"/python && sudo python3 ./setup.py install
+cd "${TVM_HOME}"/build && echo set\(USE_VITIS_AI ON\) >> config.cmake && cmake .. && make tvm_runtime -j$(nproc)
+cd "${TVM_HOME}"/python && pip3 install -e .
 
 
 DISTRIBUTION=`lsb_release -i -s`
