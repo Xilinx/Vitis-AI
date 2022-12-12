f9667af09db32755dce865ea733e7438ba8d5fa4
Zhan Su <zhan.su@amd.com>
2022-12-12 02:48:23 -0600

    custom_operator example update (#914)
    * update the package link in the board_setup
    
    * update the package link in the board_setup
    
    * update vck5000 setup link
    
    * update vai_runtime examples
    
    * add Custom_OP_Demo pytorch_example op_registration
    
    * add Custom_OP_Demo pytorch_example op_registration
    
    * update custom_operator
    
    Co-authored-by: suzhan <zhans@xilinx.com>

---
+++ examples/custom_operator/pytorch_example/op_registration/python/op_registration_cloud.sh
@@ -0,0 +1,6 @@
+sudo ln -sf /usr/lib/libvart_op_imp_python-cpu-op.so /usr/lib/libvart_op_imp_PPScatterV2.so
+<<<<<<< HEAD
+sudo cp vart_op_imp/ /usr/lib/python3/dist-packages/ -r
+=======
+sudo cp vart_op_imp/ /usr/lib/python3/site-packages/ -r
+>>>>>>> e16b559d3471611cc79411ceb92ee859f18c0266
