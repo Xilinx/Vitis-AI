0edcb6485dbf04c684be466c45de1950562ad832
Yubin Li <yubinli@amd.com>
2023-01-04 11:24:39 +0800

    delete the rnn demos (#949)

--- demos/rnn/setup/dockerfiles/banner_rnn.sh
+++
@@ -1,7 +0,0 @@
-
-echo -e "For XRNN TensorFlow Demos do:"
-echo -e "    \e[31m conda activate rnn-tf-2.0 \e[m"
-echo -e "For XRNN PyTorch Demos do:"
-echo -e "    \e[31m conda activate rnn-pytorch-1.4 \e[m"
-echo -e "For LSTM RNN Compiler and Quantizer Workflows do:"
-echo -e "    \e[31m conda activate vitis-ai-rnn \e[m"
